4f2a636719aae0b776a2199ba5c54ca5bf07a081
k-jheeta <134445049+k-jheeta@users.noreply.github.com>
2026-02-11 07:42:43 +0000

    Writing power up failing test

--- Week2-Module3/test/camera/CameraTest.java
+++ Week2-Module3/test/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,9 +10,17 @@ public class CameraTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    Sensor sensor = context.mock(Sensor.class);
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-         // Write your test here
+         Camera camera = new Camera();
+
+         context.checking(new Expectations() {{
+             exactly(1).of(sensor).powerUp();
+         }});
+
+         camera.powerOn();
     }
 
 }
